91138780cd650087350fc1da6ab786d3b910ec8e
Tristan Baughman <tristanbaughman@gmail.com>
2026-05-07 22:10:25 -0700

    removed the add name function

--- app.py
+++ app.py
@@ -13,10 +13,7 @@ title_col, logo_col = st.columns([5,1])
 
 with title_col:
     st.title("MV360 & 3U USA Analysis Dashboard")
-    name = st.text_input("Enter your name: ")
-if name:
-    st.write(f"Hello, {name}! Welcome to the MV360 & 3U USA Analysis")
-
+    
 with logo_col:
     st.image("assets/3U-Vision-USAdarksilhouette.png", width=200)
 
